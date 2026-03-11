d21bac7d16e48ccb671170abae1037de28711eef
elhamcr36-droid <elhamcr36@gmail.com>
2026-03-11 22:33:36 +0700

    Update app.py

--- salon_app.py/app.py
+++ salon_app.py/app.py
@@ -89,10 +89,10 @@ if st.session_state.page == "Home":
         st.write("💬 **LINE ID:** @222salon")
         st.write("🔵 **Facebook:** 222 Salon")
     with c2:
-      st.subheader("📍 พิกัดร้าน")
-    
-    # แทนที่ URL ด้านล่างนี้ด้วยลิงก์ที่คุณก๊อปปี้มาจาก Google Maps (แถบ Embed)
-    map_url = "https://www.google.com/maps/place/222+%E0%B8%96%E0%B8%99%E0%B8%99+%E0%B9%80%E0%B8%97%E0%B8%A8%E0%B8%9A%E0%B8%B2%E0%B8%A5+1+%E0%B8%95%E0%B8%B3%E0%B8%9A%E0%B8%A5%E0%B8%9A%E0%B9%88%E0%B8%AD%E0%B8%A2%E0%B8%B2%E0%B8%87+%E0%B8%AD%E0%B8%B3%E0%B9%80%E0%B8%A0%E0%B8%AD%E0%B9%80%E0%B8%A1%E0%B8%B7%E0%B8%AD%E0%B8%87%E0%B8%AA%E0%B8%87%E0%B8%82%E0%B8%A5%E0%B8%B2+%E0%B8%AA%E0%B8%87%E0%B8%82%E0%B8%A5%E0%B8%B2+90000/@7.1915128,100.5959263,17z/data=!3m1!4b1!4m6!3m5!1s0x304d3323c7ad029d:0x7cfb098f4f859e4c!8m2!3d7.1915128!4d100.6007972!16s%2Fg%2F11jylj3r6y?entry=ttu&g_ep=EgoyMDI2MDMwOC4wIKXMDSoASAFQAw%3D%3D"
+      with c2:
+    st.subheader("📍 พิกัดร้าน")
+    # ลิงก์ที่แปลงเป็นรูปแบบ Embed สำหรับ 222 ถนน เทศบาล 1
+    map_url = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3875.526564554363!2d100.5235!3d13.7367!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29929f60f6991%3A0x67905a76983785a0!2zMjIyIOCluCmeCmSDguYDguIrguLguOCmuCsuCmSAx!5e0!3m2!1sth!2sth!4v1710174000000!5m2!1sth!2sth"
     
     components.html(
         f'<iframe src="{map_url}" width="100%" height="230" style="border:0; border-radius:15px;" allowfullscreen="" loading="lazy"></iframe>', 
@@ -257,6 +257,7 @@ elif st.session_state.page == "ViewQueues":
         if not active.empty:
             st.table(active[['time', 'service', 'fullname']].sort_values('time'))
         else: st.info(f"ไม่มีการจองในวันนี้ ({today_str})")
+
 
 
 
